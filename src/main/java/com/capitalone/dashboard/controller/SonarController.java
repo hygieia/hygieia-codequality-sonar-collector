f22da051ff4c28cce8f5622522c84d4350a6054c
Murugan, Aravindhan <aravindhan.murugan@capitalone.com>
2020-10-28 17:33:41 -0400

    collector util class added

--- src/main/java/com/capitalone/dashboard/controller/SonarController.java
+++ src/main/java/com/capitalone/dashboard/controller/SonarController.java
@@ -2,24 +2,19 @@ package com.capitalone.dashboard.controller;
 
 import com.capitalone.dashboard.collector.SonarClient;
 import com.capitalone.dashboard.collector.SonarClientSelector;
-import com.capitalone.dashboard.model.CodeQuality;
-import com.capitalone.dashboard.model.CollectionError;
 import com.capitalone.dashboard.model.Collector;
 import com.capitalone.dashboard.model.SonarProject;
 import com.capitalone.dashboard.repository.CodeQualityRepository;
 import com.capitalone.dashboard.repository.CollectorRepository;
 import com.capitalone.dashboard.repository.SonarProjectRepository;
+import com.capitalone.dashboard.util.SonarCollectorUtil;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.validation.Valid;
 import java.util.Comparator;
@@ -32,8 +27,6 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 @Validated
 public class SonarController {
-
-    private static final Log LOG = LogFactory.getLog(SonarController.class);
 
     private final SonarProjectRepository sonarProjectRepository;
     private final CodeQualityRepository codeQualityRepository;
@@ -70,7 +63,7 @@ public class SonarController {
             if (Objects.nonNull(this.collector)) {
                 if ((Objects.nonNull(projectName) && Objects.nonNull(projectToRefresh = getExistingProject()))
                 || (Objects.nonNull(projectKey) && Objects.nonNull(projectToRefresh = createNewProjectIfNotExists()))) {
-                    updateCodeQualityData(projectToRefresh);
+                    SonarCollectorUtil.updateCodeQualityData(this.collector, this.sonarClient, projectToRefresh);
                     return response("successfully refreshed sonar project");
                 }
                 return response("unable to refresh sonar project");
@@ -110,36 +103,5 @@ public class SonarController {
                 findSonarProjectsByProjectName(this.collector.getId(), this.instanceUrl, this.projectName);
         return CollectionUtils.isNotEmpty(sonarProjects) ?
                 sonarProjects.stream().sorted(Comparator.comparing(SonarProject::getLastUpdated).reversed()).findFirst().get() : null;
-    }
-
-    private void updateCodeQualityData(SonarProject project) {
-        try {
-            CodeQuality codeQuality = this.sonarClient.currentCodeQuality(project);
-            if (codeQuality != null) {
-                project.setLastUpdated(System.currentTimeMillis());
-                project.setCollectorId(this.collector.getId());
-                sonarProjectRepository.save(project);
-                codeQuality.setCollectorItemId(project.getId());
-                codeQuality.setTimestamp(System.currentTimeMillis());
-                codeQualityRepository.save(codeQuality);
-            }
-        } catch (HttpClientErrorException e) {
-            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
-                project.setEnabled(false);
-                project.setLastUpdated(System.currentTimeMillis());
-                CollectionError error = new CollectionError("404", "disabled as the project no longer exists in Sonar");
-                project.getErrors().add(error);
-                sonarProjectRepository.save(project);
-                LOG.info(String.format("Disabled as a result of HTTPStatus.NOT_FOUND, projectName=%s instanceUrl=%s",
-                        project.getProjectName(), project.getInstanceUrl()));
-            } else {
-                LOG.error(e.getStackTrace());
-            }
-        } catch (ParseException parseEx) {
-            CollectionError error = new CollectionError("500", parseEx.getMessage());
-            project.getErrors().add(error);
-            sonarProjectRepository.save(project);
-            LOG.error(parseEx);
-        }
     }
 }
