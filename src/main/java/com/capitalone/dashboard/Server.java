dd3fd835888225c91a296aaba1634aa50c31e4bb
OSPO-CapOne <62259150+OSPO-CapOne@users.noreply.github.com>
2022-07-13 09:47:46 -0400

    Create Server.java

---
+++ src/main/java/com/capitalone/dashboard/Server.java
@@ -0,0 +1,26 @@
+package com.capitalone.dashboard;
+
+import com.capitalone.dashboard.config.MongoConfig;
+import com.capitalone.dashboard.config.RestApiAppConfig;
+import com.capitalone.dashboard.config.WebMVCConfig;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Application configuration and bootstrap
+ */
+@SpringBootApplication
+@EnableEncryptableProperties
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class, RestApiAppConfig.class, WebMVCConfig.class, MongoConfig.class);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
